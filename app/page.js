a7339c70fd2d1029f57aac92bce52e4c2e50a497
Matthew McGovern <mattmcg37@gmail.com>
2024-11-08 17:01:42 +0400

    kickstart

--- app/page.js
+++ app/page.js
@@ -1,9 +1,27 @@
+import Link from "next/link";
 
 
 export default function Home() {
   return (
     <>
-    <h1>this is the body</h1>
+    <header>
+    <div className="slideshow"></div>
+
+    <div>
+      <div className="hero">
+        <h1 className="text-4xl text-center font-bold mb-2">Younger Today, Younger Now, YoungerU</h1>
+        <p className="mb-4 text-center">Slow down or even reverse your biological age starting today.</p>
+      </div>
+      <div className="cta text-center justify-between">
+        <Link href="/community" className="mr-4">Join the Community</Link>
+        <Link href="/coaching">Explore Meal Plans</Link>
+      </div>
+    </div>
+    </header>
+
+    <main>
+
+    </main>
     </>
   );
 }
--- components/navbar.jsx
+++ components/navbar.jsx
@@ -1,28 +1,58 @@
+"use client";
+
+import { useState } from "react";
 import Image from "next/image";
-import youngerU from "../public/youngerU.png"
+import youngeru from "../public/youngeru.jpg";
+import logo from "@/public/logo.png"
+import hamburg from "../public/hamburg.png"
 import Link from "next/link";
 
-export default function navbar() {
-    return (
-    <header>
-      <Image src={youngerU} width={150} height={150} alt="Younger U" />
+export default function Navbar({ initialMenuState }) {
+    const [isOpen, setIsOpen] = useState(initialMenuState);
 
-      <nav>
-        <ul>
-            <li>
-                <Link href="/coaching">Coaching</Link>
-            </li>
-            <li>
-                <Link href="/about">About</Link>
-            </li>
-            <li>
-                <Link href="/podcast">Podcast</Link>
-            </li>
-            <li>
-                <Link href="/articles">Articles</Link>
-            </li>
-        </ul>
-      </nav>
-    </header>
-    )
+    const toggleMenu = () => {
+        setIsOpen(!isOpen);
+    };
+
+    return (
+        <header className="flex justify-between items-start">
+            <Image src={youngeru} width={150} height={150} alt="Younger U" className="mr-auto" />
+
+            <nav className="ml-auto">
+                <div className="md:hidden pt-4 pr-4" onClick={toggleMenu}>
+                    <button className=" focus:outline-none">
+                        <Image src={hamburg} alt="Menu" width={30} height={30} />
+                    </button>
+                </div>
+
+                <ul className={`flex-col pt-8 md:flex-row md:flex space-x-16 justify-end w-full pr-4 md:pr-8 lg:pr-16 ${isOpen ? 'flex' : 'hidden'} md:flex`}>
+                    <li>
+                        <Link href="/coaching">Coaching</Link>
+                    </li>
+                    <li>
+                        <Link href="/about">About</Link>
+                    </li>
+                    <li>
+                        <Image src={logo} alt="logo" width={100} height={100} className="align-middle" />
+                    </li>
+                    <li>
+                        <Link href="/podcast">Podcast</Link>
+                    </li>
+                    <li>
+                        <Link href="/articles">Articles</Link>
+                    </li>
+                </ul>
+            </nav>
+        </header>
+    );
+}
+
+export async function getServerSideProps() {
+    const initialMenuState = false;
+
+    return {
+        props: {
+            initialMenuState,
+        },
+    };
 }
