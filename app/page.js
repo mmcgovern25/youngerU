cba88c46ca75e93e9a5436f44f24be50112cdaa6
Matthew McGovern <mattmcg37@gmail.com>
2024-11-26 23:03:09 -0500

    shoutout homie claude fr

--- app/page.js
+++ app/page.js
@@ -1,15 +1,25 @@
 'use client'
+import Link from "next/link";
+
 
 export default function Home() {
   return (
     <main>
+      <div className="flex flex-col items-center min-h-screen">
+          <h1 className="text-4xl font-bold mb-4">Younger Today, Younger Now, YoungerU</h1>
+          <p className="text-xl mb-8">Empowering the next generation of fitness enthusiasts</p>
+          <Link href="/contact" className="bg-blue-500 text-white px-6 py-3 rounded-lg hover:bg-blue-600 transition duration-300">
+            Get Started
+          </Link>
+      </div>
+
       <div className="ondebox">
         <svg 
           className="onde" 
           xmlns="http://www.w3.org/2000/svg" 
           viewBox="0 24 150 28" 
           preserveAspectRatio="none" 
-          shape-rendering="auto"
+          shapeRendering="auto"
         >
           <defs>
             <path id="onda" d="M-160 44c30 0 58-18 88-18s 58 18 88 18 58-18 88-18 58 18 88 18 v44h-352Z" />
@@ -22,7 +32,6 @@ export default function Home() {
           </g>
         </svg>
       </div>
-      {/* rest of your content */}
     </main>
   );
 }
