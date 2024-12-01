5abe4b3638172472b9d66c7620bd449c42c7884b
Matthew McGovern <mattmcg37@gmail.com>
2024-12-01 17:11:27 -0500

    mobile menu tut done

--- components/NavMenu.tsx
+++ components/NavMenu.tsx
@@ -3,20 +3,28 @@
 import React, {useState} from 'react'
 import { navItems } from '../utils/NavData';
 import Link from 'next/link';
+import { motion, AnimatePresence } from "motion/react";
 
 import Spotify from "@/public/spotify.svg";
 import Instagram from "@/public/instagram.svg";
 import Youtube from "@/public/youtube.svg";
 import ApplePodcast from "@/public/applepodcast.svg";
 import TikTok from "@/public/tiktok.svg";
+import NavButton from './NavButton';
 
 const NavMenu = () => {
     const [menuOpen, setMenuOpen] = useState(false);
 
   return (
     <div className="relative block sm:hidden">
-        <button onClick={() => setMenuOpen(!menuOpen)}>Menu</button>
-        {menuOpen && (<div className="absolute -top-2 -right-2 w-64 min-h-64 rounded-md bg-neutral-50 p-8 z-10 flex flex-col">
+        <NavButton menuOpen={menuOpen} setMenuOpen={setMenuOpen} />
+        <AnimatePresence>
+        {menuOpen && (<motion.div 
+        initial={{scale: 0, opacity: 0}}
+        animate={{scale: 1, opacity: 1}}
+        exit={{scale: 0, opacity: 0}}
+        transition={{duration: 0.5, type: "spring"}}
+        className="absolute -top-2 -right-2 w-64 min-h-64 rounded-md bg-neutral-50 p-8 z-10 flex flex-col origin-top-right">
             <ul className="flex flex-col gap-2 flex-1">
                 {navItems.map(({label}) => (
                     <Link href="/" key={label}>
@@ -41,8 +49,9 @@ const NavMenu = () => {
                     <ApplePodcast className="h-5 w-5" />
                 </Link>
             </div>
-        </div>
+        </motion.div>
     )}
+    </AnimatePresence>
     </div>
   )
 }
