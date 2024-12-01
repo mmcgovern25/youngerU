535ea73ed8d2247b4135fe8eacb2e7e3314db197
Matthew McGovern <mattmcg37@gmail.com>
2024-12-01 13:51:26 -0500

    hero mobile freindly text

--- app/page.js
+++ app/page.js
@@ -9,7 +9,7 @@ import HowItWorks from '../components/howitworks'
 export default function Home() {
   return (
     <main>
-      <div className="flex flex-col items-center justify-center min-h-[80vh] -mt-20">
+      <div className="flex flex-col items-center justify-center min-h-[80vh] -mt-24">
       <motion.div
           initial={{ x: "-10vh", opacity: 0 }}
           animate={{
@@ -17,7 +17,7 @@ export default function Home() {
             opacity: [1, 1],
             transition: { duration: 3, ease: "easeOut" },
             
-  }} className="text-[#0077BC] mt-20 mb-2 text-7xl font-bold mb-4">Younger Today.</motion.div>
+  }} className="text-[#0077BC] mt-12 mb-2 text-5xl sm:text-6xl font-bold mb-4">Younger Today.</motion.div>
           <motion.div
           initial={{ x: "10vw", opacity: 0 }}
           animate={{
@@ -25,19 +25,19 @@ export default function Home() {
             opacity: [0, 1],
             transition: { duration: 3, ease: "easeOut", delay: 1 },
             
-  }} className="text-[#65B5AC] text-7xl font-bold mb-4">Younger Now.</motion.div>
+  }} className="text-[#65B5AC] text-5xl sm:text-6xl font-bold mb-4">Younger Now.</motion.div>
           <motion.div
           initial={{ opacity: 0 }}
           animate={{
             opacity: 1,
             transition: { duration: 3, delay: 3 }
-          }} className="mb-8 text-[150px] font-bold mb-4">
+          }} className="mb-8 text-[90px] sm:text-[150px] font-bold ">
             <span className="text-[#0077BC]">younger</span>
             <span className="text-[#65B5AC]">U</span>
           </motion.div>
-          <p className="text-xl mb-8">Here is a one sentence punchline that I dont know yet.</p>
+          <p className="text-md sm:text-xl mb-8 justify-center text-center">Here is a one sentence punchline that I dont know yet.</p>
           <motion.div
-      className="text-white w-[150px] h-[60px] rounded-[50px] bg-[#0077BC] cursor-pointer flex items-center justify-center"
+      className="text-white w-[130px] h-[50px] sm:w-[150px] sm:h-[60px] rounded-[50px] bg-[#0077BC] cursor-pointer flex items-center justify-center text-center text-sm sm:text-base"
       whileHover={{ scale: 1.1 }}
       whileTap={{ scale: 0.9 }}
       transition={{ type: "spring", stiffness: 400, damping: 10 }}>
@@ -45,9 +45,9 @@ export default function Home() {
       </motion.div>
       </div>
 
-      <div className="flex flex-col items-center justify-center">
-        <Link href="#how-it-works" scroll={true}>
-          <p className=''>How it works</p>
+      <div className="flex flex-col items-center justify-center -mt-8">
+        <Link href="#how-it-works">
+          <p className='mb-2'>How it works</p>
           <DownIcon className='mt-2'/>
         </Link>
       </div>
